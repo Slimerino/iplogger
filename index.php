57e814d723457a010edf18569ad9e1005e8ab681
Slimey <34658972+Slimerino@users.noreply.github.com>
2018-04-05 18:13:34 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$dateTime = date('Y/m/d G:i:s');
+$file = "admin.html"; // Change to whatever you want
+$file = fopen($file, "a");
+$data = "<center><pre><h3><b>User IP</b>: $ip <br></h3></pre></center>";
+fwrite($file, $data);
+fclose($file);
+
+echo "<center><pre style=\"color:red;\"><b>User IP</b>: $ip </pre></center>" // change this is you want
+?>
+<html>
+<head>
+    <title>I like memes</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <style>
+        @font-face { font-family: COOLVETI; src: url('COOLVETI.TTF'); }
+        h1, h2, h3, pre {
+            font-family: COOLVETI
+        }
+    </style>
+</head>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<body>
+
+<!-- Put your own HTML and CSS in here -->
+
+<center><h1 style="color:red;" class="animated infinite pulse">Thanks for your IP :)</h1></center>
+<br>
+</body>
